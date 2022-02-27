5f08575e5d926dbf224f65b306ac04130e4d07fe
Bhvrito <bhvrgav@users.noreply.github.com>
2022-02-27 15:12:16 +0100

    "Commit done"

---
+++ Add2Integers.java
@@ -0,0 +1,22 @@
+/*
+ * This programme computes the sum of 2 integers.
+ */
+
+ import java.util.Scanner;
+
+ public class Add2Integers {
+     public static void main(String[] args) {
+         int num1, num2, sum; //Declaration of variables
+         Scanner in = new Scanner(System.in); //Scans for keyboard input
+
+         System.out.print("Enter first integer: "); //Prompt user input
+         num1 = in.nextInt();
+         System.out.print("Enter second integer: "); //Prompt user input
+         num2 = in.nextInt();
+
+         sum = num1 + num2; //Compute the sum
+         System.out.println("The sum is: " + sum);
+         in.close(); //Close the scanner
+     }	
+ }
+ 
